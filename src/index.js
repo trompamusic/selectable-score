b08a41c748b23130fccbf83b23227b6116fcfc71
David M. Weigl <weigl@mdw.ac.at>
2020-01-09 16:21:32 +0100

    simplify index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import React, { Component }  from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import ReduxPromise from 'redux-promise';
-import { Router, Route, browserHistory } from 'react-router'
 
-import { reducers } from 'meld-clients-core/src/reducers';
 import TestApp from './containers/testApp';
 
-const createStoreWithMiddleware = applyMiddleware(thunk, ReduxPromise)(createStore);
-
 ReactDOM.render(
-	<Provider store={createStoreWithMiddleware(reducers)}>
-		<Router history={browserHistory}> 
-			<Route path="/" component={TestApp} />
-		</Router>
-	</Provider>
-		, document.querySelector('.container')
+	<Provider> 
+    <TestApp />
+	</Provider>, document.querySelector('.container')
 );
