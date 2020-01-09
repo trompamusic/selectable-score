dc0899c1b2ab1fd40bb3e5e704a2f624239e85c5
David M. Weigl <weigl@mdw.ac.at>
2020-01-09 17:10:56 +0100

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React, { Component }  from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import ReduxPromise from 'redux-promise';
 
+import { reducers } from 'meld-clients-core/src/reducers';
 import TestApp from './containers/testApp';
 
+const createStoreWithMiddleware = applyMiddleware(thunk, ReduxPromise)(createStore);
+
 ReactDOM.render(
-	<Provider> 
+	<Provider store={createStoreWithMiddleware(reducers)}>
     <TestApp />
-	</Provider>, document.querySelector('.container')
+	</Provider>
+		, document.querySelector('.container')
 );
