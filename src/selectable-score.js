313f0e1723358db365021c6dca4a35b1429ff58f
David M. Weigl <weigl@mdw.ac.at>
2020-07-21 17:31:12 +0200

    initial implementation of annotation retrieval (WIP)

--- src/selectable-score.js
+++ src/selectable-score.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux' ;
 import Score from 'meld-clients-core/lib/containers/score';
 import DragSelect from "dragselect/dist/DragSelect";
 import ReactDOM from 'react-dom';
+import auth from 'solid-auth-client';
+
+const CONTAINS = "http://www.w3.org/ns/ldp#contains"
 
 const defaultVrvOptions = {
   scale: 45,
@@ -25,7 +28,11 @@ class SelectableScore extends Component {
       selectorString: "selectorString" in this.props 
         ? this.props.selectorString
         : defaultSelectorString,
-      scoreComponentLoaded: false
+      scoreComponentLoaded: false,
+      updateAnnotationContainer: this.props.updateAnnotationContainer || false,
+      annotationContainerContentToRetrieve = new Set(),
+      annotationContainerContentItems = {} 
+
     }
     this.enableSelector = this.enableSelector.bind(this);
     this.scoreComponent = React.createRef();
@@ -61,6 +68,48 @@ class SelectableScore extends Component {
     });
     this.setState({selector: selector});
   }
+
+  fetchAnnotationContainer() { 
+    auth.fetch(this.props.annotationContainerUri, {
+      mode: 'cors',
+      headers: { 'Accept': 'application/ld+json' }
+    })
+    .then( response => response.json())
+    .then( (data) => { 
+      // fetch any contents of the container
+      let uri = annotationContainerUri;
+      // ensure uri ends with slash
+      uri = uri.charAt(uri.length-1) === "/" ? uri : uri + "/"
+      // find json-ld description of the container itself
+      const container = data.filter( x => x["@id"] === uri )[0]
+      if(container.includes(CONTAINS)) { 
+        const containerUris = new Set(
+          container.CONTAINS.map( contentItem => contentItem["@id"] )
+        )
+        this.setState({ annotationContainerContentToRetrieve: contentUris }, 
+          this.fetchAnnotationContainerContent()
+        )
+      } else { this.props.onReceiveAnnotationContainerContent({}) }; // report empty container
+    })
+    .catch( err => console.log("Error: ", err))
+  }
+
+  fetchAnnotationContainerContent() { 
+    this.state.annotationContainerContentToRetrieve.forEach((uri) => { 
+      this.setState({ annotationContainerContentToRetrieve: this.state.annotationContainerContentToRetrieve.delete(uri) }, 
+        auth.fetch(uri, {
+          mode: 'cors',
+          headers: { 'Accept': 'application/ld+json' }
+        })
+        .then( response => response.json())
+        .then( (data) => { 
+          this.setState({ annotationContainerContentItems: 
+            ...this.state.annotationContainerContentItems, [uri]: data  // immutable update
+          })
+        
+
+
+
   
   componentDidMount() { 
     // horrible hack to allow SVG to be loaded into DOM first
@@ -68,9 +117,19 @@ class SelectableScore extends Component {
     setTimeout(() => {
       this.enableSelector();
     }, 1000)
+
   }
 
   componentDidUpdate(prevProps, prevState) {
+    // handle fetching of annotation container contents
+    if(this.props.annotationContainerUri && this.state.updateAnnotationContainer) { 
+      console.log("Fetching anno container", this.props.annotationContainerUri);
+      if(this.props.onReceiveAnnotationContainerContent) { 
+        this.fetchAnnotationContainer()
+      } else { 
+        console.error("Specified annotation container URI without onReceiveAnnotationContainerContent callback");
+      }
+    }
     if(!prevState.scoreComponentLoaded && this.scoreComponent.current) { 
       // first load of score component - start observing for DOM changes
       this.setState({ "scoreComponentLoaded": true }, () => { 
